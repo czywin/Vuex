373334e21cc371eaf274743059f7c85b2a298f99
czywin <820128033@qq.com>
2023-02-02 10:39:31 +0800

    project init

---
+++ src/components/Substraction.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <h3>当前计数器的值为：</h3>
+        <button>-1</button>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
